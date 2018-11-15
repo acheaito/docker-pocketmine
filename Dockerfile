360e3634ab35269fa7b560da4cf76dab8459bd24
Ali Cheaito <acheaito@gmail.com>
2018-11-14 20:50:23 -0500

    try again with centos

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,14 @@
 FROM centos:7
 MAINTAINER  Ali Cheaito <acheaito@gmail.com>
 
-# Update, Install Prerequisites
-RUN yum -y update && \
-  yum install -y vim sudo wget perl gcc g++ make automake libtool autoconf m4 gcc-multilib && \  
-  yum clean all && \
-  rm -rf /var/lib/apt/lists/*
-
-# Stage Files
-COPY server.properties /tmp/server.properties
-COPY start.sh /start.sh
-RUN chmod 755 /start.sh
-
-# Setup User
 RUN useradd -d /data -s /bin/bash --uid 1000 minecraft
 
-# Setup container
-EXPOSE 19132
-VOLUME ["/data"]
-WORKDIR /data
+COPY build.sh /
+COPY server.properties /tmp/server.properties
+RUN chmod 755 /build.sh
+RUN /build.sh
 
-# Start Pocketmine
-CMD ["/start.sh"]
+#USER minecraft
+#CMD ["/data/start.sh"]
+USER root
+CMD ["/bin/bash"]
--- build.sh
+++ build.sh
@@ -1,5 +1,28 @@
-docker stop pocketmine
-docker rm pocketmine
-docker rmi -f acheaito/docker-pocketmine:latest
-docker build . --tag acheaito/docker-pocketmine:latest
-docker create -it -p 19132:19132/tcp -p 19132:19132/udp -v ~/.pocketmine/data:/data --name pocketmine acheaito/docker-pocketmine:latest
+yum install -y yum-utils epel-release http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum-config-manager --enable remi-php72
+yum update -y
+
+yum install -y sudo curl wget perl openssl gcc gcc-c++ git bison bzip2 make automake libtool autoconf m4 gcc-multilib
+
+
+#clean up
+yum clean all 
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/cache/yum
+
+# stage installer
+rm -f /data/install.sh 
+wget -q -O /data/install.sh https://get.pmmp.io
+chmod 755 /data/install.sh
+
+# check if configuration file exists
+if [ ! -e /data/server.properties ]; then
+  cp /tmp/server.properties /data/server.properties
+fi
+
+# setup owner
+chown -R minecraft:minecraft /data
+
+# install pocketmine
+cd /data
+#sudo -E -u minecraft ./install.sh -u
